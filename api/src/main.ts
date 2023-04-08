c92c69a7276ae98cadb626c02f6b27d86ab65be5
GoreevArtem <goreevartoum@gmail.com>
2023-04-08 14:58:44 +0300

    ******

---
+++ api/src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Chat example')
+    .setDescription('The chat API description')
+    .setVersion('1.0')
+    .addTag('chat')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
